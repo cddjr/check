b34fec94f96ff86b4c7835abe094eeaf05249233
cddjr <dengjingren@foxmail.com>
2022-08-18 15:56:41 +0800

    更新 网易新闻

--- dailycheckin_scripts/ck_wyxw.py
+++ dailycheckin_scripts/ck_wyxw.py
@@ -3,10 +3,10 @@
 cron: 0 8 * * *
 new Env('网易新闻');
 """
-import requests
-
 from utils import check
-from urllib3 import disable_warnings
+from urllib3 import disable_warnings, Retry
+from requests.adapters import HTTPAdapter
+import requests
 
 
 class WYXW:
@@ -14,6 +14,11 @@ class WYXW:
 
     def __init__(self, check_item):
         self.check_item = check_item
+        self.session = requests.Session()
+        adapter = HTTPAdapter()
+        adapter.max_retries = Retry(connect=3, read=3)
+        self.session.mount("https://", adapter)
+        self.session.mount("http://", adapter)
 
     def sign(self):
         headers = {
@@ -21,20 +26,19 @@ class WYXW:
             "Accept": "*/*",
             "Content-Type": "application/x-www-form-urlencoded",
             "User-Agent": "NewsApp/88.4 iOS/15.4.1 (iPhone14,3)",
-            "User-tk": self.user_tk,
-            "User-D": self.user_d,
-            "User-id": self.user_id,
+            "User-U": self.user_u
         }
-        response = requests.post(url="https://c.m.163.com/uc/api/sign/v3/commit", headers=headers, data=self.data, verify=False).json()
+        response = self.session.post(url="https://c.m.163.com/uc/api/sign/v3/commit",
+                                     headers=headers, data=self.data, verify=False).json()
         try:
             if response["code"] == 200:
                 data = response["data"]
                 serialDays = data["serialDays"]
                 awardGoldCoin = data["awardGoldCoin"]
                 awardScore = data["awardScore"]
-                subtitle = data["subtitle"] # 再签47天得「持之以恒」3级勋章
+                subtitle = data["subtitle"]  # 再签47天得「持之以恒」3级勋章
                 msg = f"签到成功, {subtitle}\n奖励金币{awardGoldCoin}，奖励积分{awardScore}\n已连续签到{serialDays}天"
-            else: # 700 重复签到
+            else:  # 700 重复签到
                 msg = response["msg"]
         except Exception as e:
             print(f"签到失败: {e}")
@@ -44,9 +48,7 @@ class WYXW:
     def main(self):
         try:
             name = self.check_item.get("name")
-            self.user_id = self.check_item.get("user_id")
-            self.user_d = self.check_item.get("user_d")
-            self.user_tk = self.check_item.get("user_tk")
+            self.user_u = self.check_item.get("user_u")
             self.data = self.check_item.get("data")
             sign_msg = self.sign()
         except Exception as e:
@@ -56,11 +58,12 @@ class WYXW:
             {"name": "帐号信息", "value": name},
             {"name": "签到信息", "value": sign_msg},
         ]
-        msg = "\n".join([f"{one.get('name')}: {one.get('value')}" for one in msg])
+        msg = "\n".join(
+            [f"{one.get('name')}: {one.get('value')}" for one in msg])
         return msg
 
 
-@check(run_script_name="网易新闻",run_script_expression="WYXW")
+@check(run_script_name="网易新闻", run_script_expression="WYXW")
 def main(*args, **kwargs):
     return WYXW(check_item=kwargs.get("value")).main()
 
@@ -68,4 +71,3 @@ def main(*args, **kwargs):
 if __name__ == "__main__":
     disable_warnings()
     main()
-

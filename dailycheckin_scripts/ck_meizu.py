8a82ee62e3e75688d995761691a5d8103aaa8382
cddjr <dengjingren@foxmail.com>
2022-12-22 16:48:39 +0800

    更新 魅族

--- dailycheckin_scripts/ck_meizu.py
+++ dailycheckin_scripts/ck_meizu.py
@@ -21,79 +21,38 @@ class Meizu:
         self.session.mount("https://", adapter)
         self.session.mount("http://", adapter)
 
-    def sign(self, cookie):
+    def user(self, cookie):
         headers = {
-            "authority": "bbs-act.meizu.cn",
             "pragma": "no-cache",
             "cache-control": "no-cache",
             "accept": "application/json, text/javascript, */*; q=0.01",
             "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.74",
-            "origin": "https://bbs.meizu.cn",
-            "referer": "https://bbs.meizu.cn/",
+            "origin": "https://www.meizu.cn",
+            "referer": "https://www.meizu.cn/",
             "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
             "cookie": cookie,
         }
-        params = (
-            ("mod", "signin"),
-            ("action", "sign"),
-        )
-        response = self.session.get(url="https://bbs-act.meizu.cn/index.php", headers=headers, params=params).json()
-        msg = response.get("message")
-        return msg
+        response = self.session.get(
+            url="https://myplus-api.meizu.cn/myplus-muc/u/user/v2", headers=headers).json()
+        data = response.get("data", {})
+        return data["nickname"], data["mucUserId"]
 
-    def draw(self, cookie, count: int = 0):
+    def sign(self, cookie):
         headers = {
-            "authority": "bbs-act.meizu.cn",
+            "pragma": "no-cache",
+            "cache-control": "no-cache",
             "accept": "application/json, text/javascript, */*; q=0.01",
-            "x-requested-with": "XMLHttpRequest",
             "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.74",
-            "content-type": "application/x-www-form-urlencoded",
-            "origin": "https://bbs-act.meizu.cn",
-            "referer": "https://bbs-act.meizu.cn/2/index.html",
-            "accept-language": "zh-CN,zh;q=0.9",
+            "origin": "https://www.meizu.cn",
+            "referer": "https://www.meizu.cn/",
+            "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
             "cookie": cookie,
         }
-        draw_data = {"mod": "index", "action": "draw", "id": "2"}
-        award_list = []
-        success_count = 0
-        error_count = 0
-        if count:
-            for i in range(count):
-                try:
-                    data = self.session.post(url="https://bbs-act.meizu.cn/index.php", headers=headers, data=draw_data).json()
-                    if data["code"] == 200:
-                        one_msg = data.get("data", {}).get("award_name")
-                        award_list.append(one_msg)
-                        success_count += 1
-                    else:
-                        if data["code"] == 205:
-                            one_msg = "没有中奖" # data.get("message")
-                        elif data["code"] == 402:
-                            one_msg = "机会用完"
-                            error_count = count - success_count - error_count
-                            break
-                        else:
-                            print(data.get("code"), data.get("message"))
-                            one_msg = "抽奖失败"
-                        error_count += 1
-                except Exception as e:
-                    one_msg = f"抽奖出错: {e}"
-                    error_count += 1
-                print(f"第{i + 1}次抽奖结果：" + str(one_msg))
-                randomSleep(min=3)
-            msg = f"成功抽奖 {success_count} 次"
-            draw_msg = [
-                {"name": "抽奖信息", "value": str(msg)},
-                {"name": "抽奖结果", "value": ";".join(award_list)},
-            ]
-        else:
-            draw_msg = [
-                {"name": "抽奖信息", "value": "未开启抽奖"},
-            ]
-        data = {"mod": "index", "action": "get_user_count", "id": "2"}
-        user_info = self.session.post("https://bbs-act.meizu.cn/index.php", headers=headers, data=data).json()
-        uid = user_info.get("data", {}).get("uid")
-        return draw_msg, uid
+        response = self.session.post(
+            url="https://myplus-api.meizu.cn/myplus-muc/u/user/signin", headers=headers).json()
+        # {"code":1012000000,"msg":"今日已签到","timestamp":1671698782431,"data":null}
+        msg = response.get("msg")
+        return msg
 
     def main(self):
         meizu_cookie = self.check_item.get("cookie")
@@ -103,12 +62,15 @@ class Meizu:
             print("初始化抽奖次数失败: 重置为 0 ", str(e))
             draw_count = 0
         sign_msg = self.sign(cookie=meizu_cookie)
-        draw_msg, uid = self.draw(cookie=meizu_cookie, count=draw_count)
+        nick, uid = self.user(cookie=meizu_cookie)
+        # draw_msg, uid = self.draw(cookie=meizu_cookie, count=draw_count)
         msg = [
-                  {"name": "帐号信息", "value": uid},
-                  {"name": "签到信息", "value": sign_msg},
-              ] + draw_msg
-        msg = "\n".join([f"{one.get('name')}: {one.get('value')}" for one in msg])
+            {"name": "帐号ID", "value": uid},
+            {"name": "帐号昵称", "value": nick},
+            {"name": "签到信息", "value": sign_msg},
+        ]  # + draw_msg
+        msg = "\n".join(
+            [f"{one.get('name')}: {one.get('value')}" for one in msg])
         return msg
 
 
